e2e613a9f2aca2ec6324fa9fe4ac384a1f0696ae
Sai Akhil Aloor <saiakhil.aloor7@gmail.com>
2017-02-11 13:22:31 +0530

    Added news reader foe reading news

--- ndtv-headlines.py
+++ ndtv-headlines.py
@@ -8,11 +8,11 @@ from mutagen.mp3 import MP3
 
 def reader(s):
     text = gTTS(text=s, lang='en')
-    text.save("news.mp3")                  #saving as mp3 file
-    player = vlc.MediaPlayer("news.mp3")       #loading to media player
+    text.save("news.mp3")                  
+    player = vlc.MediaPlayer("news.mp3")       
     file = MP3("news.mp3")
-    n = file.info.length                         #getting duration of sound file
-    player.play()                                  #Playing sound
+    n = file.info.length                        
+    player.play()                                  
     time.sleep(n+2.5) 
 
 url="http://www.ndtv.com/top-stories"
@@ -20,7 +20,7 @@ page=requests.get(url)
 html=BeautifulSoup(page.content,'html.parser')
 a=html.find('div',{'class':'new_storylising'})
 b=a.find_all('div',{'class':'nstory_header'})
-reader("Welcome to NDTV news, today's top stories are ")
+reader("Welcome, Today's top stories are ")
 for i in range(len(b)):
 	subprocess.Popen(['notify-send',"Today's Top Stories",b[i].text])
 	x=b[i].text
